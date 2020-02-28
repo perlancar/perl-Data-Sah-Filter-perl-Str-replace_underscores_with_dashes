28f7890096f858f7986de81eea07b6f55be00b54
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-28 12:48:09 +0700

    Initial commit

---
+++ lib/Data/Sah/Filter/perl/Str/replace_underscores_with_dashes.pm
@@ -0,0 +1,41 @@
+package Data::Sah::Filter::perl::Str::replace_underscores_with_dashes;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'Replace underscores in string with dashes',
+    };
+}
+
+sub filter {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+    $res->{expr_filter} = join(
+        "",
+        "do { my \$tmp = $dt; \$tmp =~ s/_/-/g; \$tmp }",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
+
+
+=head1 prepend:SEE ALSO
+
+L<Data::Sah::Filter::perl::Str::replace_dashes_with_underscores>
